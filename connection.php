7f1b1f4bf26a599026c9cf12209a6c51bff8cb55
Neeraj Ajithkumar <hello@neerajajithkumar.com>
2023-02-27 00:18:15 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,11 +1,11 @@
 <?php
 date_default_timezone_set('Asia/Kolkata');
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "charity";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
+$servername = "db-mysql-blr1-00436-do-user-12232507-0.b.db.ondigitalocean.com";
+$username = "doadmin";
+$password = "AVNS_sRVnWmkZg6brEHdfOMw";
+$dbname = "defaultdb";
+$port = "25060";
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
