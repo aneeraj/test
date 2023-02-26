acdfc8cf216aab0aeab7470ba6d6ae036248ca44
Neeraj Ajithkumar <hello@neerajajithkumar.com>
2023-02-27 01:03:17 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -5,7 +5,9 @@ $username = "doadmin";
 $password = "AVNS_sRVnWmkZg6brEHdfOMw";
 $dbname = "defaultdb";
 $port = "25060";
-$conn = new mysqli($servername, $username, $password, $dbname, $port);
+$conn = mysqlii_init();
+$conn->ssl_set(null, null, 'ca-certificate.crt', null, null);
+$conn->real_connct($servername, $username, $password, $dbname, $port);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
